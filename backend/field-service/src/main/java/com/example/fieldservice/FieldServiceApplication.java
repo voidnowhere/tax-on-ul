9c93c2f059b3776634009ec8d5b3ef4583491bb8
voidnowhere <alivnw@outlook.com>
2024-01-08 15:22:18 +0100

    git init

---
+++ backend/field-service/src/main/java/com/example/fieldservice/FieldServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.fieldservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldServiceApplication.class, args);
+	}
+
+}
---
+++ backend/field-service/src/test/java/com/example/fieldservice/FieldServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fieldservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
